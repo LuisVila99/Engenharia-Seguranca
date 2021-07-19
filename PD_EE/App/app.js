80cce176290f358ae20ea27262db48bcf6237b81
LuisVila99 <luispedrovila@gmail.com>
2021-07-19 12:30:34 +0100

    html changes

--- PD_EE/App/app.js
+++ PD_EE/App/app.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
 app.engine('html', require('ejs').renderFile);
 
 
